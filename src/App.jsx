0f9c1db434c50a61f9b3e8ecbc18a579422ed725
Satyam368 <sandeepjagadhri75@gmail.com>
2026-02-05 17:27:58 +0530

    feat: initialize React project with Vite, ESLint, and basic structure
    - Added package.json with dependencies for React, Vite, and ESLint.
    - Created initial React components and styles in App.jsx and App.css.
    - Included SVG assets for React and Vite logos.
    - Set up global styles in index.css for consistent theming.
    - Configured Vite with a proxy for API requests.
    - Established main entry point in main.jsx for rendering the application.

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>hello world </h1>
+      <h2>this is my first world web deploy on aws ec2</h2>
+      <h3>student at chitkara university</h3>
+    </div>
+  )
+}
+
+export default App
